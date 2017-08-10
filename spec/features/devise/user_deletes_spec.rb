b884b2fe37d75e32c12aa7f5e67d1bc97d96246a
andy2012v <andy2012v@gmail.com>
2017-08-10 17:13:28 -0400

    changes for rake log in

--- spec/features/devise/user_deletes_spec.rb
+++ spec/features/devise/user_deletes_spec.rb
@@ -9,7 +9,7 @@ feature "account deletion" do
     end
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_on 'Log in'
+    click_on 'Log In'
 
     click_on "Edit Profile"
     fill_in 'Email', with: 'newemail@example.com'
